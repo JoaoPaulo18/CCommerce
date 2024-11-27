8dccae8576f34a63fa55eeccbdadd559c5a48bd7
João Paulo <devjoaomoreira@gmail.com>
2024-11-26 21:31:04 -0300

    Test 2

--- src/Elements/Start/Start.jsx
+++ src/Elements/Start/Start.jsx
@@ -72,7 +72,7 @@ const Start = () => {
         <li className='two-row'>
           <h2>Jeans</h2>
           <p>Style and comfort meet in our collection of jeans. Discover the latest trends and perfect cuts for an impeccable look.</p>
-          <img className={styles.Jeans} src="../../Images/Jeans.png" alt="" />
+          <img className={styles.Jeans} src="../../../Images/Jeans.png" alt="" />
         </li>
 
         <li>
